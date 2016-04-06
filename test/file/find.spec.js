fc62310a2896fdacdffa6ef0fce1637d042b3a99
MRW Neundorf <m.neundorf@live.de>
2016-04-06 09:51:39 +0200

    fix file find and file find spec

--- test/file/find.spec.js
+++ test/file/find.spec.js
@@ -1,8 +1,30 @@
 'use strict';
 
+const path = require('path');
+
 describe('file:find', function() {
-    it.skip('should return an object with files in fixtures folder', function () {
-        //const files = this.lib.file.find(`${__dirname}/fixtures`);
-        //console.log(files);
+    const fixturesPath = path.resolve(__dirname, 'fixtures');
+
+    const fixtureFiles = [
+        path.resolve(fixturesPath, '.foo'),
+        path.resolve(fixturesPath, '.bar'),
+        path.resolve(fixturesPath, 'foobar.js'),
+        path.resolve(fixturesPath, 'foobar.json'),
+
+        path.resolve(fixturesPath, 'sub', '.foo'),
+        path.resolve(fixturesPath, 'sub', '.foo'),
+
+        path.resolve(fixturesPath, 'sub', 'sub', '.bar'),
+        path.resolve(fixturesPath, 'sub', 'sub', '.bar')
+    ];
+
+    it('should return an object with files in fixtures folder', function () {
+        const files = this.lib.file.find(fixturesPath, {
+            recursive: true
+        });
+
+        fixtureFiles.forEach(file => {
+           files.should.have.property(file);
+        });
     });
 });
